9b1acf7dd4c7fc09413ef77bc9dac0c6d852e307
Wikode Medias <wikodemedias@gmail.com>
2020-05-29 15:24:17 +0200

    added redux and connected header component to global store with redux, + sign in actions

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfilDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-      // si on se connecte alors userAuth = object; sinon = null
       if (userAuth) {
-        const userRef = await createUserProfilDocument(userAuth); // Cette fonction retourne l'objet userRef qui possède les méthodes de manipulation de la database si le user existe déjà dans firestore, sinon il créer un doc qui lui correspond et retourne ensuite sa ref
+        const userRef = await createUserProfilDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState({
+          setCurrentUser({
             currentUser: {
               id: snapShot.id,
               ...snapShot.data()
             }
           })
-
-          console.log(this.state)
         })
-
       } else {
-        this.setState({ currentUser: userAuth }); // si pas reçu de userAuth avec un user dedans (null) alors on remet le state en null 
+        setCurrentUser(userAuth);
       }
     })
   }
@@ -49,7 +41,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
@@ -60,4 +52,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
