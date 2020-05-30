6252903d6696204a1da411eb5a09e7097e0a5d29
Wikode Medias <wikodemedias@gmail.com>
2020-05-30 13:04:34 +0200

    cart icon and dropdown with cart reducer and action to toggle dropdown on clic

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
+const composeEnhancers = (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose);
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
 
 export default store;
