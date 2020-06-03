5e2db9bad83870d6e18890bd28e71f05904fad74
Wikode Medias <wikodemedias@gmail.com>
2020-06-03 18:25:23 +0200

    selectors and new structure for shop

---
+++ src/redux/shop/shop.selectors.js
@@ -0,0 +1,19 @@
+import { createSelector } from 'reselect';
+
+const selectShop = state => state.shop;
+
+export const selectCollections = createSelector(
+    [selectShop],
+    shop => shop.collections
+)
+
+// Récupérer les clés de notre objet collections du store dans un format array
+export const selectCollectionsForPreview = createSelector(
+    [selectCollections],
+    collections => Object.keys(collections).map(key => collections[key])
+)
+
+export const selectCollection = collectionUrlParam => createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
+)
